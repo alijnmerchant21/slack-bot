005cd7eb5a7c016591d20d3d2ad68a9fa0d52577
Aliasgar Merchant <amercha1@stevens.edu>
2023-06-13 16:42:28 +0200

    minor update

--- core/priotization/script.js
+++ core/priotization/script.js
@@ -1,42 +1,64 @@
-let tasks = [];
+// Define the tasks array
+const tasks = [];
 
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
+// Function to add a new task
+function addTask(name, priority, deadline, duration, repetition) {
+  tasks.push({
+    name,
+    priority,
+    deadline,
+    duration,
+    repetition
+  });
+}
 
-  const taskName = document.getElementById('task-name').value;
+// Function to display the tasks in a table
+function displayTasks() {
+  const taskList = document.getElementById('task-list');
+  taskList.innerHTML = '';
+
+  tasks.forEach((task) => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    const priorityCell = document.createElement('td');
+    const deadlineCell = document.createElement('td');
+    const durationCell = document.createElement('td');
+    const repetitionCell = document.createElement('td');
+
+    nameCell.textContent = task.name;
+    priorityCell.textContent = task.priority;
+    deadlineCell.textContent = task.deadline;
+    durationCell.textContent = task.duration;
+    repetitionCell.textContent = task.repetition;
+
+    row.appendChild(nameCell);
+    row.appendChild(priorityCell);
+    row.appendChild(deadlineCell);
+    row.appendChild(durationCell);
+    row.appendChild(repetitionCell);
+
+    taskList.appendChild(row);
+  });
+}
+
+// Add an event listener to the form's submit event
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Retrieve the values of the form fields
+  const name = document.getElementById('task-name').value;
   const priority = document.getElementById('priority').value;
   const deadline = document.getElementById('deadline').value;
   const duration = document.getElementById('duration').value;
   const repetition = document.getElementById('repetition').value;
 
-  const task = {
-    name: taskName,
-    priority: priority,
-    deadline: deadline,
-    duration: duration,
-    repetition: repetition
-  };
+  // Add the new task to the tasks array
+  addTask(name, priority, deadline, duration, repetition);
 
-  tasks.push(task);
+  // Display the tasks in the table
+  displayTasks();
 
-  // Sort the tasks based on priority, deadline, and duration
-  tasks.sort((a, b) => {
-    // Sort by priority
-    if (a.priority !== b.priority) {
-      return b.priority - a.priority;
-    }
-
-    // If priority is the same, sort by deadline
-    const aDeadline = new Date(a.deadline).getTime();
-    const bDeadline = new Date(b.deadline).getTime();
-    if (aDeadline !== bDeadline) {
-      return aDeadline - bDeadline;
-    }
-
-    // If deadline is the same, sort by duration
-    return a.duration - b.duration;
-  });
-
-  // Reset the form
-  document.querySelector('form').reset();
+  // Reset the form fields
+  form.reset();
 });
